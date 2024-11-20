05bb9040bbbd1d8aa2a6e6dcbe3c54f0e52895a7
RyuuAz <119066009+RyuuAz@users.noreply.github.com>
2024-11-20 14:23:44 +0100

    fix inverser l'affichage du triangle

--- Q5.java
+++ Q5.java
@@ -2,7 +2,7 @@ public class Q5
 {
   public static void main(args[])
   {
-    for (int i = 0; i < args[0]; i++)
+    for (int i = args[0]; i > 0; i--)
       for (int j = 0; j < args[0]; j++)
       {
         System.out.print("*");

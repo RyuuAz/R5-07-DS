75926d761193c10afcf7e5351c1131427afa7356
RyuuAz <119066009+RyuuAz@users.noreply.github.com>
2024-11-20 14:33:23 +0100

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,12 @@
+public class Q6
+{
+  public static void main(args[])
+  {
+    for (int i = args[0]; i > 0; i--)
+      for (int j = 0; j < args[0]; j++)
+      {
+        System.out.print("X");
+      }
+    System.out.println();
+  }
+}

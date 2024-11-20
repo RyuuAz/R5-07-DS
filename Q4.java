2adea230da97dc46564c1941bef4158003e9640f
RyuuAz <119066009+RyuuAz@users.noreply.github.com>
2024-11-20 14:06:35 +0100

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,12 @@
+public class Q4
+{
+  public static void main(args[])
+  {
+    for (int i = 0; i < args[0]; i++)
+      for (int j = 0; j < args[0]; j++)
+      {
+        System.out.print("*");
+      }
+    System.out.println();
+  }
+}
